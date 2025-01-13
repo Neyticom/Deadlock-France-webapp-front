b88cdcd2f0e028df9bbaa7ac3e0c3f44a6e68cf6
Zack-Mimir <Zack-Mimir@github.com>
2025-01-13 11:39:11 +0100

    feat: implement Hero route with controller, CRUD logic, and error handling

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
 import mainController from '../controllers/mainController';
+import heroRoutes from './heroRoutes';
+import errorHandler from '../middlewares/errorHandler';
 
 const router = Router();
 
 router.get('/status', mainController.getStatus);
+
+router.use('/heroes', heroRoutes);
+
+router.use(errorHandler);
 
 export default router;
