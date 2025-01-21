092befd18956df3ec6058f17432f8f10ac7c5990
Zack-Mimir <Zack-Mimir@github.com>
2025-01-21 14:12:40 +0100

    feat: enhance user and log controllers with role deletion and improved filtering

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -8,10 +8,10 @@ userRoutes.get('/', userController.getAllUsers);
 userRoutes.get('/:id', userController.getUserById);
 userRoutes.post('/', userController.createUser);
 userRoutes.patch('/:id', userController.updateUser);
-userRoutes.delete('/:id', userController.deleteUser);
 
 // Routes pour la gestion des rôles
 userRoutes.get('/:id/role', userController.getUserRole);
 userRoutes.patch('/:id/role', userController.updateUserRole);
+userRoutes.delete('/:id/role', userController.deleteUserRole);
 
 export default userRoutes;
